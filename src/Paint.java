9457bd3f02e9967da8920cb9deff8f3ae948de09
mirjamdusmezer <s80372@htw-dresden.de>
2022-05-17 09:55:55 +0200

    add material children

---
+++ src/Paint.java
@@ -0,0 +1,25 @@
+public class Paint extends Material{
+
+    static private double limit = 0.02;
+    private int numberOfCoats;
+    private double squareMetersPerLimit;
+
+    public Paint(String name, double price, int numberOfCoats, double squareMetersPerLimit){
+        super(name, price);
+        this.numberOfCoats = numberOfCoats;
+        this.squareMetersPerLimit = squareMetersPerLimit;
+    }
+
+    public int getNumberOfCoats(){
+        return this.numberOfCoats;
+    }
+
+    public double getSquaredMetersPerLimit(){
+        return this.squareMetersPerLimit;
+    }
+
+    @Override
+    public int getMaterialRequrirements(Surface surface){
+        
+    }
+}
