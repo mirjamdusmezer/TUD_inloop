43e7892332e29fa6004d7abe7d1eb66f57d2da02
mirjamdusmezer <s80372@htw-dresden.de>
2022-05-17 09:59:18 +0200

    add Flooring

--- src/Flooring.java
+++ src/Flooring.java
@@ -1,3 +1,19 @@
 public class Flooring extends Material{
-    
+
+    private static double limit = 0.02;
+    private double widthOfFlooring;
+
+    public Flooring(String name, double price, double width){
+        super(name, price);
+        this.widthOfFlooring = width;
+    }
+
+    public double getWidth(){
+        return this.widthOfFlooring;
+    }
+
+    @Override
+    public int getMaterialRequrirements(Surface surface){
+
+    }
 }
