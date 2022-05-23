ae91f33476df74236a4b60b396aca79267939e97
mirjamdusmezer <s80372@htw-dresden.de>
2022-05-23 19:14:40 +0200

    add methods to ResourceType

--- Desktop/src/ResourceType.java
+++ Desktop/src/ResourceType.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class ResourceType {
-    
+    private String description;
+    private KeywordCollector collector;
+
+    public ResourceType(String desc, KeywordCollector collector){
+        desc = description;
+        this.collector = collector;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public KeywordCollector getCollector() {
+        return this.collector;
+    }
 }
