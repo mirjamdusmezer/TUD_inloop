3d5535e6f736d91adc6a765156d53e1068c87ed4
mirjamdusmezer <s80372@htw-dresden.de>
2022-05-23 19:03:17 +0200

    add methods to resource class

--- Desktop/src/Resource.java
+++ Desktop/src/Resource.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
 public class Resource {
-    
+    private String name;
+    private String path;
+    private ResourceType type;
+
+    public Resource(String name, String path, ResourceType rt){
+        if (name.isEmpty() || path.isEmpty())
+            throw new IllegalArgumentException();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
 }
