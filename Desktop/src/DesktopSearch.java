50cfc4572eeba35232cd8c28e67419d23a7c402b
mirjam <72745070+mirjamdusmezer@users.noreply.github.com>
2022-05-23 18:51:02 +0200

    add methods

--- Desktop/src/DesktopSearch.java
+++ Desktop/src/DesktopSearch.java
@@ -1,3 +1,27 @@
 public class DesktopSearch {
-    
+    private Map<String, ResourceType> types;
+
+    public DesktopSearch(){
+
+    }
+
+    public void registerType(String extension, ResourceType type){
+
+    }
+
+    public ResourceType getType(String extension){
+
+    }
+
+    public void unregisterType(String extension){
+
+    }
+
+    public void registerResource(Resource res){
+
+    }
+
+    public List<Resource> processRequest(String request){
+        
+    }
 }
