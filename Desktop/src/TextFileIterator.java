13230da000081c0de0ba9c16fad91dda39d52e70
mirjamdusmezer <s80372@htw-dresden.de>
2022-05-24 07:38:47 +0200

    small change

--- Desktop/src/TextFileIterator.java
+++ Desktop/src/TextFileIterator.java
@@ -1,3 +1,25 @@
-public class TextFileIterator {
-    
+import java.util.*;
+
+public class TextFileIterator implements Iterator {
+
+    public TextFileIterator(Resource res) {
+
+        this.res = res;
+    }
+
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    public Object next() {
+        return iterator.next();
+    }
+
+    public void remove(){
+        iterator.remove();
+    }
+
+    public String getAsString(Resource res){
+        
+    }
 }
